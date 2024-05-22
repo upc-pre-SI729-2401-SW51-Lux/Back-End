97b40c091ce6f4152d2dd9aca0972497d35d9bc3
Daniel Del Castillo <danielmdcb974@gmail.com>
2024-05-21 20:37:44 -0500

    chore:initial commit.

---
+++ src/main/java/com/lux/agroges/AgrogesApplication.java
@@ -0,0 +1,13 @@
+package com.lux.agroges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrogesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrogesApplication.class, args);
+    }
+
+}
