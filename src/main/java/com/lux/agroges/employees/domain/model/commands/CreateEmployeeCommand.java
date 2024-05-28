eae9c3a2cccc353d6a203b64849ac76d6cbe0473
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-05-28 18:09:57 -0500

    fix(create-employee-command): fixed type of variable of salary and age.

--- src/main/java/com/lux/agroges/employees/domain/model/commands/CreateEmployeeCommand.java
+++ src/main/java/com/lux/agroges/employees/domain/model/commands/CreateEmployeeCommand.java
@@ -1,4 +1,4 @@
 package com.lux.agroges.employees.domain.model.commands;
 
-public record CreateEmployeeCommand(String id, String name, String lastName, String workPosition, Number salary, String phone, Number age, String state, String email) {
+public record CreateEmployeeCommand(String id, String name, String lastName, String email, String workPosition, Integer salary, String phone, Integer age, String state) {
 }
