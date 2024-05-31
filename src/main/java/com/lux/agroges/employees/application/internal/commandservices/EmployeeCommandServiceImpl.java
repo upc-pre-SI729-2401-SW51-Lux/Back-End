0812947af3183f97b77ba7eeae8d3e04c4c0f7a5
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-05-31 17:29:55 -0500

    feat(DeleteEmployeeCommand): added command to delete a employee by its id.

--- src/main/java/com/lux/agroges/employees/application/internal/commandservices/EmployeeCommandServiceImpl.java
+++ src/main/java/com/lux/agroges/employees/application/internal/commandservices/EmployeeCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.lux.agroges.employees.application.internal.commandservices;
 
 import com.lux.agroges.employees.domain.model.aggregates.Employee;
 import com.lux.agroges.employees.domain.model.commands.CreateEmployeeCommand;
+import com.lux.agroges.employees.domain.model.commands.DeleteEmployeeCommand;
 import com.lux.agroges.employees.domain.model.valueobjects.EmployeeDetails;
 import com.lux.agroges.employees.domain.services.EmployeeCommandService;
 import com.lux.agroges.employees.infrastructure.persistence.jpa.repositories.EmployeeRepository;
@@ -26,6 +27,18 @@ public class EmployeeCommandServiceImpl implements EmployeeCommandService {
         var employee = new Employee(command.firstName(), command.lastName(), command.email(), command.idDocument(), command.workPosition(), command.salary(), command.phone(), command.age(), command.photoUrl());
         employeeRepository.save(employee);
         return Optional.of(employee);
+    }
+
+    @Override
+    public void handle(DeleteEmployeeCommand command) {
+        if (!employeeRepository.existsById(command.id())) {
+            throw new IllegalArgumentException("Employee does not exists.");
+        }
+        try {
+            employeeRepository.deleteById(command.id());
+        } catch (Exception e){
+            throw new IllegalArgumentException("Error while deleting course: " + e.getMessage());
+        }
     }
 }
 
--- src/main/java/com/lux/agroges/employees/domain/services/EmployeeCommandService.java
+++ src/main/java/com/lux/agroges/employees/domain/services/EmployeeCommandService.java
@@ -2,9 +2,11 @@ package com.lux.agroges.employees.domain.services;
 
 import com.lux.agroges.employees.domain.model.aggregates.Employee;
 import com.lux.agroges.employees.domain.model.commands.CreateEmployeeCommand;
+import com.lux.agroges.employees.domain.model.commands.DeleteEmployeeCommand;
 
 import java.util.Optional;
 
 public interface EmployeeCommandService {
     Optional<Employee> handle(CreateEmployeeCommand command);
+    void handle(DeleteEmployeeCommand command);
 }
