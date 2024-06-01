4b3ab6a948a25fd25b67c8d9d601745e639d7b30
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-05-31 19:22:45 -0500

    feat(EmployeeRepository): added decoration repository.

--- src/main/java/com/lux/agroges/employees/infrastructure/persistence/jpa/repositories/EmployeeRepository.java
+++ src/main/java/com/lux/agroges/employees/infrastructure/persistence/jpa/repositories/EmployeeRepository.java
@@ -4,9 +4,11 @@ import com.lux.agroges.employees.domain.model.aggregates.Employee;
 import com.lux.agroges.employees.domain.model.valueobjects.EmailAddress;
 import com.lux.agroges.employees.domain.model.valueobjects.EmployeeName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmail(EmailAddress email);
     Optional<Employee> findByName(EmployeeName name);
