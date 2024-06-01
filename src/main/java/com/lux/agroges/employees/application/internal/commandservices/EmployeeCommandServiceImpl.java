f2f2d17884200233acb5d2c3b8046d316cc7cb7f
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-05-31 19:31:21 -0500

    feat(UpdateEmployeeDetailsCommand): added command to update the details of the employee except for its id document.

--- src/main/java/com/lux/agroges/employees/application/internal/commandservices/EmployeeCommandServiceImpl.java
+++ src/main/java/com/lux/agroges/employees/application/internal/commandservices/EmployeeCommandServiceImpl.java
@@ -3,6 +3,7 @@ package com.lux.agroges.employees.application.internal.commandservices;
 import com.lux.agroges.employees.domain.model.aggregates.Employee;
 import com.lux.agroges.employees.domain.model.commands.CreateEmployeeCommand;
 import com.lux.agroges.employees.domain.model.commands.DeleteEmployeeCommand;
+import com.lux.agroges.employees.domain.model.commands.UpdateEmployeeDetailsCommand;
 import com.lux.agroges.employees.domain.model.valueobjects.EmployeeDetails;
 import com.lux.agroges.employees.domain.services.EmployeeCommandService;
 import com.lux.agroges.employees.infrastructure.persistence.jpa.repositories.EmployeeRepository;
@@ -38,6 +39,19 @@ public class EmployeeCommandServiceImpl implements EmployeeCommandService {
             employeeRepository.deleteById(command.id());
         } catch (Exception e){
             throw new IllegalArgumentException("Error while deleting course: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Optional<Employee> handle(UpdateEmployeeDetailsCommand command) {
+        var result = employeeRepository.findByIdDocument(command.idDocument());
+        if (result.isEmpty()) throw new IllegalArgumentException("Employee does not exists.");
+        var employeeDetailsToUpdate = result.get();
+        try {
+            var updatedEmployeeDetails = employeeRepository.save(employeeDetailsToUpdate.updateEmployeeDetails(command.workPosition(), command.salary(), command.phone(), command.age(), command.photoUrl()));
+            return Optional.of(updatedEmployeeDetails);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while updating employee details " + e.getMessage());
         }
     }
 }
