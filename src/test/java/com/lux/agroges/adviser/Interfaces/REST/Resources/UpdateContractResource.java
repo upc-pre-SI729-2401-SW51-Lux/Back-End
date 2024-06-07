790f63753830f345883fc05fcf70e84049885db0
GustavoPoma <157068395+GustavoPoma@users.noreply.github.com>
2024-06-07 12:27:08 -0500

    Delete src/test/java/com/lux/agroges/adviser directory
    chore: remove non-functional advise directory
    
    Eliminate the advise bounded context as it is no longer needed and was causing issues.

--- src/test/java/com/lux/agroges/adviser/Interfaces/REST/Resources/UpdateContractResource.java
+++
@@ -1,4 +0,0 @@
-package com.lux.agroges.adviser.Interfaces.REST.Resources;
-
-public record UpdateContractResource(String companyName, String contractorDni, Integer applicationTime, Double averageSalary, String description, String status) {
-}
