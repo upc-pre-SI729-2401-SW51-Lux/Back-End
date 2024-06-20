732dce81b936bcca279a2ed51131bdd438053de2
Sergio Joel Pecan Pariona <145626151+SergioPecan@users.noreply.github.com>
2024-06-20 08:50:44 -0500

    Update SwaggerRedirectController.java

--- src/main/java/com/lux/agroges/web/SwaggerRedirectController.java
+++ src/main/java/com/lux/agroges/web/SwaggerRedirectController.java
@@ -1,14 +1,21 @@
 package com.lux.agroges.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Controller
-public class SwaggerRedirectController {
+@Configuration
+public class MyConfiguration {
 
-    @GetMapping("/")
-    public RedirectView redirectToSwagger() {
-        return new RedirectView("/swagger-ui.html");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
